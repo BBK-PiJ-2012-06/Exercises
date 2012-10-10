0ae870aa03e3f78475eff89fc31b69967e9097f6
Simon Ward <simon.ward42@gmail>
2012-10-10 17:17:42 +0100

    added menu and code for i/o to methods. fixed an error in decimal2binary(): if condition to determine whether to append '1' or '0' to the binary number was incorrect

--- binaryDecimal.groovy
+++ binaryDecimal.groovy
@@ -2,30 +2,55 @@ int binary2decimal(String binary){
     int decimal
     int powerOfTwo = binary.length() - 1
     for(int i=0; i<binary.length(); i++){
-        decimal += binary.charAt(i) * Math.pow(2, powerOfTwo)
+        decimal += Integer.parseInt(binary.substring(i,i+1)) * Math.pow(2, powerOfTwo)
         powerOfTwo--
     }
     return decimal
 }
 
 String decimal2binary(int decimal){
-    String binary
+    String binary = ""
     // Find highest power of two that divides decimal
     int powerOfTwo = 0
-    boolean finding = true
     while(decimal >= Math.pow(2,powerOfTwo+1)){
         powerOfTwo++
     }
     // Now convert decimal to binary, starting with the highest power of two just found
-    while(decimal){
-        if(decimal / Math.pow(2,powerOfTwo) > 0){
-            binary.add('1')
+    for(powerOfTwo; powerOfTwo>=0; powerOfTwo--){
+        if(decimal >= Math.pow(2,powerOfTwo) ){
+            binary += "1"
             decimal -= Math.pow(2, powerOfTwo)
-        }else{
-            binary.add('0')   
         }
-        powerOfTwo--
+        else{
+            binary += "0"   
+        }
     }
     return binary
 }
 
+boolean running = true
+while(running){
+    println "What would you like to do?"
+    println "1 - Convert binary to decimal"
+    println "2 - Convert decimal to binary"
+    println "0 - Exit"
+    print ">"
+    int option = Integer.parseInt(System.console().readLine())
+    switch(option){
+        case 0: running = false
+                break
+        case 1: println "Please enter a binary number:"
+                print ">"
+                String binary = System.console().readLine()
+                int decimal = binary2decimal(binary)
+                println "The binary number " + binary + " in decimal is " + decimal + "\n"
+                break
+        case 2: println  "Please enter a decimal number:"
+                print ">"
+                int decimal = Integer.parseInt(System.console().readLine())
+                String binary = decimal2binary(decimal)
+                println "The decimal number " + decimal + " in binary is " + binary + "\n"
+                break
+       default: println "Invalid option, please try again."
+    }
+}
